2f5b317a033512d1d23b66b45dabe10130a99685
Simon Stucki <simon.stucki@goldinteractive.ch>
2018-04-05 07:08:51 +0200

    Base reducer implementation

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,6 +1,54 @@
+import statusCode from '../base/statusCode';
+import {
+  resetting,
+  pending,
+  rejected,
+  fulfilled,
+  status,
+  succeed,
+  fail,
+  update,
+} from '../base';
+
+const emptyState = () => ({
+  data: null,
+  status: status(),
+});
+
+const defaultReducer = TYPE => (state = emptyState(), action) => {
+  const {
+    payload,
+  } = action;
+  switch (action.type) {
+  case resetting(TYPE):
+    return {
+      ...state,
+      data: null,
+      status: status(),
+    };
+  case fulfilled(TYPE):
+    return {
+      ...state,
+      data: payload.data,
+      status: statusCode.isOk(payload.status) ? succeed(payload.operation) : fail(payload),
+    };
+  case pending(TYPE):
+    return {
+      ...state,
+      status: update(),
+      data: null,
+    };
+  case rejected(TYPE):
+    return {
+      ...state,
+      status: fail(payload),
+      data: null,
+    };
+  default:
+    return state;
+  }
+};
 
 export default {
-  defaultReducer: () => {
-
-  },
+  defaultReducer,
 };
